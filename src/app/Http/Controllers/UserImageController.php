87080ac8d06b77f1183be6c489cebebf230cef5f
takao kazuhito <simasyamu@gmail.com>
2024-11-08 20:40:17 +0900

    画像アプロード時に画像を圧縮する（プロフィール画像）15

--- src/app/Http/Controllers/UserImageController.php
+++ src/app/Http/Controllers/UserImageController.php
@@ -39,7 +39,7 @@ class UserImageController extends Controller
                     );
                     $image_paths[] = $directory . '/' . $filename;
                 } elseif (app('env') == 'production') {
-                    $path = Storage::disk('s3')->put('users/' . $filename, $resize_img, 'public'); //S3バケットのusersフォルダに、圧縮した画像をファイル名指定して保存
+                    $path = Storage::disk('s3')->put('/users/' . $filename, $resize_img); //S3バケットのusersフォルダに、圧縮した画像をファイル名指定して保存
                     $url = Storage::disk('s3')->url($path); //直前に保存した画像のS3上で付与されたurlを取得 https://~
                     $image_paths[] = $url;
                 } elseif (app('env') == 'testing') {
